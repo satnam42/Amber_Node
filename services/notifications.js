64c5385c80d7dd83df8195ceda2fd61ad8dea337
satnam <fullstact.mspl@gmail.com>
2022-02-22 15:35:43 +0530

    notification fixs

--- services/notifications.js
+++ services/notifications.js
@@ -41,21 +41,34 @@ const sendCallNotification = async (body, context) => {
         throw new Error('called  user  device Token not found')
     }
 
-    let payload = {
-        data: {  //you can send only notification or only data(or include both)
-            "channelName": body.channelName.toString(),
-        },
+    // let payload = {
+    //     data: {  //you can send only notification or only data(or include both)
+    //         "channelName": body.channelName.toString(),
+    //     },
+    //     notification: {
+    //         title: "call",
+    //         body: body.username
+    //     },
+
+    // };
+    // const options = {
+    //     priority: "high",
+    //     timeToLive: 60 * 60 * 24
+    // };
+
+    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)        
+        to: user.deviceToken,
         notification: {
             title: "call",
             body: body.username
         },
-
-    };
-    const options = {
-        priority: "high",
-        timeToLive: 60 * 60 * 24
-    };
-    await admin.messaging().sendToDevice(user.deviceToken, payload, options)
+        data: {  //you can send only notification or only data(or include both)            
+            "channelName": body.channelName.toString(),
+        }
+    }
+    const res = await admin.messaging().send(message)
+    const res = await admin.messaging().send(message)
+    log.info(res)
     log.end()
     // .then(response => {
     //     console.log('message Successfully sent :', response);
