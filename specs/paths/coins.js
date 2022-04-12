6ced2026b98a1c0461565072433a1a7894739fc8
satnam <fullstact.mspl@gmail.com>
2022-04-12 18:03:01 +0530

    paging fixes

--- specs/paths/coins.js
+++ specs/paths/coins.js
@@ -47,6 +47,20 @@ module.exports = [
                     required: true,
                     type: "string"
                 },
+                {
+                    in: "query",
+                    type: "integer",
+                    name: "pageNo",
+                    description: "pageNo",
+                    required: true
+                },
+                {
+                    in: "query",
+                    type: "integer",
+                    name: "pageSize",
+                    description: "pageSize",
+                    required: true
+                },
 
             ],
             responses: {
