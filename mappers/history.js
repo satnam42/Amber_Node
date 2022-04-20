bb93cd5658873a839804a50bcb7dd930e830a9fa
satnam <fullstact.mspl@gmail.com>
2022-04-20 15:24:50 +0530

    history api fixes

--- mappers/history.js
+++ mappers/history.js
@@ -4,10 +4,9 @@ const imageUrl = require('config').get('image').url
 exports.toModel = entity => {
 
     const model = {
-        fromUser: entity.fromUser.firstName,
         toUser: entity.toUser.firstName,
         toUserAvatar: entity.toUser.avatar ? `${imageUrl}${entity.toUser.avatar}` : "",
-        fromUserAvatar: entity.entity.fromUser.avatar ? `${imageUrl}${entity.entity.fromUser.avatar}` : "",
+        fromUserAvatar: entity.fromUser.avatar ? `${imageUrl}${entity.fromUser.avatar}` : "",
         callType: entity.callType,
         duration: entity.duration,
         time: entity.time,
